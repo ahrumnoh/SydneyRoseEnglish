fc25e67a0f9b014aa32129ea055699bef0e17d1d
ahrum noh <sydneyroseenglish@gmail.com>
2023-05-15 10:04:00 +0900

    updated
    updated

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,7 +17,7 @@ let availableQuestions = []
 
 //Setup Timer
 
-var time=60;
+var time=600;
 var x=setInterval(function() {
     document.getElementById("timer-count").innerHTML="" + time ;
     time = time-1;
@@ -34,52 +34,89 @@ var x=setInterval(function() {
 
 let questions = [
     {
-        question: "Which one is a right sentence grammatically?",
-        choice1: "Shelly has a problems.",
-        choice2: "Paul speaking English very well.",
-        choice3: "Have you be Canada, before?",
-        choice4: "What color is your car",
-        answer: 4,    
-    },
-    {
-        question: "Which one is correct?",
-        choice1: "James did not meet Brian yesterday.",
-        choice2: "How old you are?",
-        choice3: "I have not finish my homework, yet",
-        choice4: "Are you student?",
+        question: "다음 중 변형3타가 아닌 것은? ",
+        choice1: "participate",
+        choice2: "refuse",
+        choice3: "forget",
+        choice4: "ask",
         answer: 1,    
     },
     {
-        question: "Choose a right sentence!",
-        choice1: "I staying at home tonight.",
-        choice2: "Jerry will probably be on the beach tomorrow.",
-        choice3: "I does not think Kelly will pass the exam",
-        choice4: "James used to swimming every day.",
+        question: "Please pick a correct sentence",
+        choice1: "Lisa has arrived 10 minutes ago",
+        choice2: "I was calling to Tom when James and Miranda were arrival using the train near the station",
+        choice3: "Did you playing the soccer?",
+        choice4: "James prefers to drink a cup of coffee every morning ",
+        answer: 4,    
+    },
+    {
+        question: "변형1자가 아닌것은?",
+        choice1: "Explode",
+        choice2: "belong",
+        choice3: "comply",
+        choice4: "look",
+        answer: 1,    
+    },
+    {
+        question: "순수1자가 아닌것은?",
+        choice1: "leave",
+        choice2: "Become",
+        choice3: "wait",
+        choice4: "Agree",
+        answer: 3,    
+    },
+    {
+        question: "4타 + '시.수.태', 두 가지 조건을 만족시키는 것을 고르시오",
+        choice1: "I was listening Jazz every single day",
+        choice2: "Tom gave a gift to me yesterday",
+        choice3: "My parents watching TV when Jack calling us last night",
+        choice4: "Brian gave me a cup of coffee at the ST store last night",
+        answer: 4,    
+    },
+    {
+        question: "Please find a correct irregular verb format?",
+        choice1: "run - run - run",
+        choice2: "break - broke - broken",
+        choice3: "fall - fell - falling",
+        choice4: "eat - ate- eaten",
+        answer: 4,    
+    },
+    {
+        question: "Please find a correct sentence",
+        choice1: "David just arrived",
+        choice2: "David has just arrived",
+        choice3: "David has just been arriving",
+        choice4: "David has just arriving",
         answer: 2,    
     },
     {
-        question: "Which sentence is correct?",
-        choice1: "Where are you come from?",
-        choice2: "Tom's father are in hospital.",
-        choice3: "My teacher is very kind.",
-        choice4: "my key losing yesterday.",
+        question: "변형1자가 아닌것은?",
+        choice1: "turn",
+        choice2: "belong",
+        choice3: "need",
+        choice4: "deal",
         answer: 3,    
     },
-
     {
-    //new insert, just in case, remove it
-        question: "I have to waering glasses",
-        choice1: "The office cleaned yesterday",
-        choice2: "Are these rooms painting every day?",
-        choice3: "Do you always get up early?",
-        choice4: "Maria speaking four languages.",
-        answer: 3,    
-    }  
-
+        question: "Past-Present의 3번째 용법을 고르시오",
+        choice1: "The computer has been repaired for an hour",
+        choice2: "Jack has built his first computing protocol for data science since last year ",
+        choice3: "We have just moved here",
+        choice4: "look! Charlie has arrived now!",
+        answer: 2,    
+    },
+    {
+        question: "일반3타를 고르시오",
+        choice1: "I study English",
+        choice2: "Jane is 18 years old",
+        choice3: "Do I need to find a job?",
+        choice4: "What is wrong?",
+        answer: 1,    
+    }
 ]
 
 const SCORE_POINTS = 100
-const MAX_QUESTIONS = 5
+const MAX_QUESTIONS = 10
 
 
 
